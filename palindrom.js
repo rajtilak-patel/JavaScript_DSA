c86c2feab2437c67f226cabd736d7b2d6b188e02
rajtilak-patel <rp2380264@gmail.com>
2023-01-18 21:34:40 +0530

    palindrome string

--- palindrom.js
+++ palindrom.js
@@ -8,4 +8,20 @@ const palindrom = (str) =>{
     }
 }
 
+const palindromicString = (str) =>{
+     let i=0;
+     let j = str.length-1;
+
+     while(i<j){
+        if(str[i]===str[j]){
+            i++;
+            j--;
+        }
+        else{
+            return false;
+        }
+     }
+     return true;
+}
+console.log(palindromicString("naman"));
 console.log(palindrom("abbaabba"));
