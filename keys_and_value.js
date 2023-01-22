4810b481148f7a1e1bee00ac7521510b379699eb
rajtilak-patel <rp2380264@gmail.com>
2023-01-22 22:23:04 +0530

    added keys value questions

---
+++ keys_and_value.js
@@ -0,0 +1,9 @@
+let obj = { id: "1", name: "Test User", age: "25", profession: "Developer" };
+
+console.log(Object.keys(obj));
+
+console.log(Object.values(obj));
+
+console.log(Object.entries(obj));
+
+
