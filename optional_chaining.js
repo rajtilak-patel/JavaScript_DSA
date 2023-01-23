1459b9b801b65ecb2b8cb18d17f9970cee7275af
rajtilak-patel <rp2380264@gmail.com>
2023-01-23 23:15:13 +0530

    optional chaning

---
+++ optional_chaining.js
@@ -0,0 +1,24 @@
+console.log("Welcome to Programiz!");
+
+// const user = {
+//     name:"rajtilak",
+//     age:22,
+//     address:{
+//         city:"raiput",
+//         pin:493663
+//     }
+// };
+
+// console.log(user.name);
+// console.log(user.address.city);
+// console.log(user.address.house);
+// console.log(user.address.house?.road);
+
+const user = [
+    {name:"raj",age:25},
+    {name:"makalu",age:45},
+    {name:"bharosha",age:22}
+]
+
+// console.log(user[3].name);
+console.log(user[3]?.name);
