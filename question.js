2bf68a0e83c65a25d0e14da4868dde50ffb23ecf
rajtilak-patel <rp2380264@gmail.com>
2023-01-24 22:11:09 +0530

    added hosting

---
+++ question.js
@@ -0,0 +1,17 @@
+console.log("varName",varName);
+var varName;
+console.log("varName",varName);
+varName = "captaine america";
+console.log("varName",varName);
+
+
+fn();
+function fn(){
+    console.log("this is a function");
+}
+fn()
+fnContainer();
+function fnContainer(){
+    console.log("this is container  function");
+}
+fnContainer()
