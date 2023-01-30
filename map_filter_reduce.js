714574c3512573d3bd43d80742c300feb85ff475
rajtilak-patel <rp2380264@gmail.com>
2023-01-30 22:08:11 +0530

    filter use

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -2,8 +2,17 @@ const arr = [5,1,3,2,6];
 
 // map :- it use to iterate every element and return a new arr; map pass the argument in array
 
-const output = arr.map((x)=>{
-    return x*2;
+// const output = arr.map((x)=>{
+//     return x*2;
+// })
+
+// console.log(output);
+
+
+// filter - filter is to use filter a function for sum condition;
+
+const output = arr.filter(function(x){
+     return x>3;
 })
 
 console.log(output);
