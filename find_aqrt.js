4299ed19fe80e0fde5847923cddf1adeccf7be66
rajtilak-patel <rp2380264@gmail.com>
2023-02-03 21:43:32 +0530

    added

---
+++ find_aqrt.js
@@ -0,0 +1,22 @@
+console.log( Math.sqrt(50));
+
+console.log(Math.pow(2,4));
+
+console.log(Math.round(2.82));
+
+console.log(Math.floor(5.89));
+
+console.log(Math.ceil(5.19));
+
+console.log(Math.max(15,18));
+
+console.log(Math.min(15,18));
+
+console.log(Math.abs(45-78));  //positive value
+
+
+
+
+
+
+
