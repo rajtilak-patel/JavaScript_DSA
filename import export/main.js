17ce658fe1a715ab3b6d7c036139a11bbc240bef
rajtilak-patel <rp2380264@gmail.com>
2023-02-08 23:50:43 +0530

    added import export

---
+++ import export/main.js
@@ -0,0 +1,5 @@
+import { message, value , multiply } from "./module.js";
+
+console.log(message);
+console.log(value);
+console.log(multiply(20,10));
---
+++ import export/module.js
@@ -0,0 +1,14 @@
+
+
+const message = "hello world";
+
+const value = 100;
+
+function multiply(a,b){
+      return a*b;
+}
+
+
+
+// export value from another function
+export { message , value , multiply};
